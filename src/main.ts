91586b26b8ff8ce368558ccfc6771dc9cc3c0dbe
Arath <ar90we17@gmail.com>
2025-05-06 23:46:53 -0700

    Trabajo en progreso, Unidades y view Unidades

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import './assets/main.css'
 import './assets/styles/global.css'; // Importa los estilos globales
+import { createVuetify } from 'vuetify';
+import 'vuetify/styles'; // Importa los estilos de Vuetify
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
 
 
 import { createApp } from 'vue'
@@ -8,8 +12,13 @@ import { createPinia } from 'pinia'
 import App from './BasePage.vue'
 import router from './router'
 
-const app = createApp(App)
+const vuetify = createVuetify({
+  components,
+  directives,
+});
 
+const app = createApp(App)
+app.use(vuetify);
 app.use(createPinia())
 app.use(router)
 
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import landing from '../views/landingViews/LandingView.vue' // Ensure this path matches the actual file location
+import landing from '@/views/landingViews/LandingView.vue' // Ensure this path matches the actual file location
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -12,22 +12,25 @@ const router = createRouter({
     {
       path: '/acerca-de',
       name: 'about',
-      component: () => import('../views/landingViews/AboutView.vue'),
+      component: () => import('@/views/landingViews/AboutView.vue'),
     },
     {
       path: '/registro',
       name: 'register',
-      component: () => import('../views/authViews/RegisterView.vue'),
+      component: () => import('@/views/authViews/RegisterView.vue'),
     },
     {
       path: '/InicioSesion',
       name: 'login',
-      component: () => import('../views/authViews/LogInView.vue'),
+      component: () => import('@/views/authViews/LogInView.vue'),
     },
     {
       path: '/dashboard',
-      name: 'dashboard',
-      component: () => import('../views/dashboardViews/DashboardView.vue'),
+
+      component: () => import('@/views/dashboardViews/DashboardView.vue'),
+      children: [
+          {path: 'unidades', name: 'dashboard', component: () => import('@/features/dashboard/views/UnidadesView.vue')},
+      ]
     }
   ],
 })
